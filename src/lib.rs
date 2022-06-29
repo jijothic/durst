0170a7e8a66b32ffd7a384702945c2b140cb900f
jijothic <georg.jijothic@gmail.com>
2022-06-29 14:04:55 +0300

    brainwork

--- src/lib.rs
+++ src/lib.rs
@@ -3,9 +3,28 @@ struct Durst {
 
 struct MachineSet{}
 
+struct DurstBuilder {
+    descriptors: Vec<MachineSet>
+}
+
+impl Default for BurstBuilder {
+    fn default() -> self {
+        DurstBuilder {
+            descriptors: Vec::new(), 
+        }
+    }
+}
 impl Durst {
-    pub fn new() -> Self {}
     
-    pub fn create_set(&mut self, description: MachineSet){}
+    pub fn add_set(&mut self, description: MachineSet){}
 
     pub fn run()
+}
+
+fn main() {
+    let mut b = DurstBuilder::default;
+    b.add_set(MachineSet::new("t2.micro", "ami-e1aa89b", |ssh| {
+        ssh.exec("sudo yum install vim");
+    }));
+    b.run(||)
+}
